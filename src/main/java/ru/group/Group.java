5363139f81c82350675ad13998225c432b604293
osadcaaalena77 <osadcaaalena77@gmail.com>
2025-11-25 20:08:39 +0300

    изменение файла Group

--- src/main/java/ru/group/Group.java
+++ src/main/java/ru/group/Group.java
@@ -1,5 +1,15 @@
 package ru.group;
 
 public class Group {
+    private String groupName;
+    private String countOfUsers;
+    private String[] users;
+
+    public Group(String groupName, String countOfUsers, String[] users) {
+        this.groupName = groupName;
+        this.countOfUsers = countOfUsers;
+        this.users = users;
+    }
 
 }
+
