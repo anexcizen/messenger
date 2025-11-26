8b9533cd958d59dc7887dbac66815a09105883e4
Diana Muhametzyanova <dianamuhametzanova1790@gmail.com>
2025-11-26 12:50:39 +0300

    База данных обновлена 2

--- src/main/java/ru/service/Database.java
+++ src/main/java/ru/service/Database.java
@@ -6,7 +6,7 @@ import ru.user.User;
 import ru.user.UserChat;
 
 public class Database {
-    public static User anita = new User("Тимлид Анита", "19:12", "босс", "12.07.2019", "+7800553535", "12.12.2007");
+    public static User anita = new User("Тимлид Анита", "19:12", "босс", "12.07.2019", "+7800553535", "12.12.2007"); // pppppp
     public static User alena = new User("Алёна", "14:25", "айпадкид", "08.11.2020", "+7800663535", "09.09.2007");
     public static User diana = new User("Диана", "21:52", "танцуй танцулька", "15.12.2019", "+7800773535", "12.04.2007");
     public static User kristina = new User("Кристина", "23:18", "работяга", "23.06.2020", "+7800883535", "11.10.2007");
