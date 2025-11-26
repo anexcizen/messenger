06d3fe59b092ce9dfd12fe62eaa68ba4b1fb9eaf
Christina <kristinakraynova71@gmail.com>
2025-11-26 13:27:14 +0300

    добавлена пара каналов в базу данных

--- src/main/java/ru/service/Database.java
+++ src/main/java/ru/service/Database.java
@@ -10,7 +10,7 @@ public class Database {
     public static User alena = new User("Алёна", "14:25", "айпадкид", "08.11.2020", "+7800663535", "09.09.2007");
     public static User diana = new User("Диана", "21:52", "танцуй танцулька", "15.12.2019", "+7800773535", "12.04.2007");
     public static User kristina = new User("Кристина", "23:18", "работяга", "23.06.2020", "+7800883535", "11.10.2007");
-    public static ChannelChat[] news = {new ChannelChat("Казань на Максималках", 317800), new ChannelChat("Новости ИТИС", 5720)};
+    public static ChannelChat[] news = {new ChannelChat("Казань на Максималках", 317800), new ChannelChat("Новости ИТИС", 5720), new ChannelChat("Лучшие люди", 1230400), new ChannelChat("Секреты успешных людей", 3715678)};
     public static Group[] groups = {new Group("Инфа проект",4,  new String[]{anita.userName, alena.userName, diana.userName, kristina.userName}), new Group("рабы", 3, new String[]{alena.userName, diana.userName, kristina.userName}), new Group( "ДУшнилы", 3, new String[]{anita.userName, alena.userName, diana.userName})};
     public static UserChat[] chats = {new UserChat(anita), new UserChat(alena), new UserChat(diana), new UserChat(kristina)};
 }
