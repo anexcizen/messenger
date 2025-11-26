cb4750672b68b4a99ef310e2008180800186064d
osadcaaalena77 <osadcaaalena77@gmail.com>
2025-11-26 13:03:42 +0300

    изменение файла

--- src/main/java/ru/group/Group.java
+++ src/main/java/ru/group/Group.java
@@ -5,6 +5,18 @@ public class Group {
     private int countOfUsers;
     private String[] members;
 
+    public String getGroupName() {
+        return groupName;
+    }
+
+    public int getCountOfUsers() {
+        return countOfUsers;
+    }
+
+    public String[] getMembers() {
+        return members;
+    }
+
     public Group(String groupName, int countOfUsers, String[] members) {
         this.groupName = groupName;
         countOfUsers = members.length;
