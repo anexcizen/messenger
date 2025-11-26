ad86af06bcda446e8afdecc9dde5994417b8bd0a
Christina <kristinakraynova71@gmail.com>
2025-11-26 13:49:10 +0300

    изменён ник пользователя

--- src/main/java/ru/service/Database.java
+++ src/main/java/ru/service/Database.java
@@ -8,7 +8,7 @@ import ru.user.UserChat;
 public class Database {
     public static User anita = new User("Тимлид Анита", "19:12", "босс", "12.07.2019", "+7800553535", "12.12.2007"); // pppppp
     public static User alena = new User("Алёна", "14:25", "айпадкид", "08.11.2020", "+7800663535", "09.09.2007");
-    public static User diana = new User("Диана", "21:52", "танцуй танцулька", "15.12.2019", "+7800773535", "12.04.2007");
+    public static User diana = new User("Вы", "21:52", "танцуй танцулька", "15.12.2019", "+7800773535", "12.04.2007");
     public static User kristina = new User("Кристина", "23:18", "работяга", "23.06.2020", "+7800883535", "11.10.2007");
     public static ChannelChat[] news = {new ChannelChat("Казань на Максималках", 317800), new ChannelChat("Новости ИТИС", 5720), new ChannelChat("Лучшие люди", 1230400), new ChannelChat("Секреты успешных людей", 3715678)};
     public static Group[] groups = {new Group("Инфа проект",4,  new String[]{anita.userName, alena.userName, diana.userName, kristina.userName}), new Group("рабы", 3, new String[]{alena.userName, diana.userName, kristina.userName}), new Group( "ДУшнилы", 3, new String[]{anita.userName, alena.userName, diana.userName})};
