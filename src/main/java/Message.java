6582b7dd4a7f78e0b6414d3a5130e3ac6951e616
Christina <kristinakraynova71@gmail.com>
2025-11-26 11:59:43 +0300

    добавлен родительский класс для сообщений всех типов (лс, группы, каналы)

--- src/main/java/Message.java
+++ src/main/java/Message.java
@@ -4,7 +4,7 @@ public class Message {
     String date;
     String time;
 
-    protected Message(String chatName, String text, String date, String time) {
+    protected Message(String chatName, String text, String date, String time) { //конструктор
         this.chatName = chatName;
         this.text = text;
         this.date = date;
