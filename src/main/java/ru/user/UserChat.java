9ea63e5c7ddb2ba4a0cba6265c6ee61819335eb6
anita <anitatalbieva@gmail.com>
2025-11-26 15:16:41 +0500

    реализовала геттеры в класс UserChat

--- src/main/java/ru/user/UserChat.java
+++ src/main/java/ru/user/UserChat.java
@@ -1,10 +1,19 @@
 package ru.user;
 
 public class UserChat {
-    String name;
-    String info;
+    private String name;
+    private String info;
+
     public UserChat(User person) {
         name = person.userName;
         info = person.lastSeen;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getInfo() {
+        return info;
     }
 }
